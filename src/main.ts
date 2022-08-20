903aa8aa3c9572787fcb12df598048f72e44b350
pinshengjuan <pinshengjuan@gmail.com>
2022-08-20 16:23:37 +0800

    feat: 🎸 add a notification when checksum has been copied

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as vscode from 'vscode';
 import checkAlgorithm from "./checkAlgorithm";
 import copyToClipboard from "./copyToClipboard";
 import strProcess from "./utils/strProcess";
@@ -15,6 +16,11 @@ function main(filePath: string, type: string) {
     const checksumNum = algorithmRet(contents, type);
     const checksumStr: string = strProcess(checksumNum);
     copyToClipboard(checksumStr).then(() => {
+      const vscConfig = vscode.workspace.getConfiguration("get-file-checksum");
+      if(vscConfig["checksumCopiedNotification"])
+      {
+        vscode.window.showInformationMessage('The checksum value for a single file has been copied to the clipboard!');
+      }
       console.log("All done");
     });
   });
